c3037722f707e976601ccb51d1de7cf0a720ba2a
amir hossein saberi <amirhosseinsaberi1376@gmail.com>
2025-03-30 00:13:07 +0430

    Initial Commit

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -17,8 +18,10 @@ const ProductDetailsPage = async (props: {
     <>
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
-          <div className="col-span-2"></div>
           <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
+          <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
               <p>
                 {product.brand}
